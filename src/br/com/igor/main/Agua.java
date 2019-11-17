d4a62d0cacd659efea626695220018c6ff0b60d5
Igor de Castro <igor.cbs@gmail.com>
2019-11-16 23:53:17 -0300

    classes Agua, Fogoe Vento

--- src/br/com/igor/main/Agua.java
+++ src/br/com/igor/main/Agua.java
@@ -1,5 +1,56 @@
 package br.com.igor.main;
 
-public class Agua {
+import java.util.Random;
+/**
+ * A classe Agua administra o tipo de Pokemon 
+ * @author igor
+ *
+ */
+public class Agua extends Pokemons{
+
+	/**
+	 * Construtor que recebe nome, tipo, nivel e experiencia do pokemon pela caixa de texto
+	 * @param nome
+	 * @param tipo
+	 * @param nivel
+	 * @param exp
+	 */
+	Agua(String nome, int tipo, int nivel, int exp) {
+		super(nome, tipo, nivel, exp);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean chanceDeCapturar() {
+		// TODO Auto-generated method stub
+		
+		double aux = 0.0;
+		aux = random(0.0, 100.0);
+		
+		if(aux > 20.0) {
+			return true;
+		}else {
+			return false;
+		}
+		
+		
+	}
+	/**
+	 * O método random randomiza um numero recenbendo dois numero como parametro
+	 * @param min
+	 * @param max
+	 * @return o numero escolhido no metodo randomico
+	 */
+	public static double random(double min, double max) {
+		Random random = new Random();
+		
+		double aux = max - min;
+		double scaled = random.nextDouble() * aux;
+		double shifted = scaled + min;
+		double finalValue = Math.round(shifted * 100.0) / 100.00;
+		
+		return finalValue;		
+		
+	}
 
 }
