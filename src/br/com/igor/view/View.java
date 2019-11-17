c9f5d67b25851dee92560e000d1da4edeea16a43
Igor de Castro <igor.cbs@gmail.com>
2019-11-17 00:09:53 -0300

    View

--- src/br/com/igor/view/View.java
+++ src/br/com/igor/view/View.java
@@ -1,5 +1,24 @@
 package br.com.igor.view;
 
-public abstract class View {
+import javax.swing.JOptionPane;
 
+public abstract class View {
+	
+	public static String getString(String msg) {
+		String aux = JOptionPane.showInputDialog(null, msg);
+		return aux;
+	}
+	
+	public static int getInt(String msg) {
+		int aux = Integer.parseInt(JOptionPane.showInputDialog(null, msg));
+		return aux;
+	}
+	
+	public static void getMsg(String msg) {
+		JOptionPane.showMessageDialog(null, msg); 
+	}
+	
+	public static void getMsgError(String msg) {
+		JOptionPane.showMessageDialog(null, msg, null, JOptionPane.INFORMATION_MESSAGE);
+	}
 }
