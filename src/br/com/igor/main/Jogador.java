a587d503ac3ef1c2be80b01da1d59ed87671556a
Igor de Castro <igor.cbs@gmail.com>
2019-11-16 21:20:52 -0300

    classes Jogador e Pokemons

--- src/br/com/igor/main/Jogador.java
+++ src/br/com/igor/main/Jogador.java
@@ -1,5 +1,121 @@
 package br.com.igor.main;
+import java.util.Arrays;
 
+/**
+ * A Classe Jogador administra jogadores do sistema
+ * @author igor
+ *
+ */
 public class Jogador {
-
+	
+	private String nome;
+	private String senha;
+	protected int contPokemon;
+	Pokemons pokemons[] = {};
+	
+	/**
+	 * Esse Construtor recebe uma string do nome do jogador, a senha do jogador e o pokemon primario do jogador
+	 * @param nome
+	 * @param senha
+	 * @param pokemon
+	 */
+	Jogador(String nome, String senha, Pokemons pokemon){
+		setNome(nome);
+		setSenha(senha);
+		setPokemons(pokemon);
+		setContPokemon(1);
+	}
+	/**
+	 * Esse método retorna o nome do jogador
+	 * @return nome do jogador
+	 */
+	public String getNome() {
+		return nome;
+	}
+	/**
+	 * Esse método adiciona um nome digitado na caixa de texto e atribui a nome
+	 * @param Recebe o nome do valor digitado
+	 */
+	public void setNome(String nome) {
+		if(nome.isBlank()) {
+			throw new IllegalArgumentException("Nome não pode ser vazio!");
+		}else {
+			this.nome = nome;
+		}
+	}
+	/**
+	 * Esse método retorna a senha do Jogador
+	 * @return senha do jogador
+	 */
+	public String getSenha() {
+		return senha;
+	}
+	
+	
+	/**
+	 * Esse método adiciona uma senha ao jogador
+	 * @param senha
+	 */
+	public void setSenha(String senha) {
+		
+		if(senha.isBlank()) {
+			throw new IllegalArgumentException("Senha não pode ser vazia!");
+		}else {
+			this.senha = senha;
+		}
+	}
+	/**
+	 * Esse método permite ao jogador capturar pokemons e adicionar em seu perfil
+	 * @param pokemon Pokemon que deseja capturar
+	 */
+	public void capturar(Pokemons pokemon) {
+		
+		if(contPokemon < 3) {
+			setPokemons(pokemon);
+		}else {
+			throw new IllegalArgumentException("Limite de pokemons excedido para capturar!");
+		}
+		
+	}
+	/**
+	 * Esse método permite adicionar uma contagem de pokemon
+	 * @param contPokemon Valor atribuido a contagem do pokemon
+	 */
+	public void setContPokemon(int contPokemon) {
+		this.contPokemon += contPokemon;
+	}
+	/**
+	 * Esse método adiciona pokemons ao jogador
+	 * @param pokemons 
+	 */
+	public void setPokemons(Pokemons pokemons) {
+		
+		if(contPokemon < 3) {
+			this.pokemons[contPokemon] = pokemons;
+			setContPokemon(contPokemon);
+		}else {
+			throw new IllegalArgumentException("Limite de Pokemons excedido!");
+		}
+		
+		
+	}
+	/**
+	 * Esse método retorna a contagem de Pokemons do Jogador
+	 * @return contagem de Pokemons
+	 */
+	public int getContPokemon() {
+		return contPokemon;
+	}
+	/**
+	 * Esse método mostra as informações do Jogador
+	 * @return uma String com as informações do Jogador
+	 */
+	@Override
+	public String toString() {
+		return "Jogador [nome=" + nome + ", senha=" + senha + ", contPokemon=" + contPokemon + ", pokemons="
+				+ Arrays.toString(pokemons) + "]";
+	}
+	
+	
+	
 }
--- src/br/com/igor/main/Pokemons.java
+++ src/br/com/igor/main/Pokemons.java
@@ -1,5 +1,121 @@
 package br.com.igor.main;
+/**
+ * A classe Pokemons serve para administrar pokemons no sistema
+ * @author igor
+ *
+ */
+abstract public class Pokemons {
+	
+	protected String nome;
+	protected int tipo;
+	protected int nivel;
+	protected int exp;
+	
+	/**
+	 * Esse Construtor recebe um nome para o pokemon, o tipo dele, o  nível e a experiencia
+	 * @param nome
+	 * @param tipo
+	 * @param nivel
+	 * @param exp
+	 */
+	Pokemons(String nome, int tipo, int nivel, int exp) {
+		setNome(nome);
+		setTipo(tipo);
+		setNivel(nivel);
+		setExp(exp);
+	}
+	/**
+	 *  Esse método abstrato tem a funcao de dar uma chance de capturar um pokemon
+	 * @return um valor se capturou um pokemon
+	 */
+	public abstract int chanceDeCapturar();
+	
+	/**
+	 * Esse método retorna o nome do pokemon
+	 * @return nome 
+	 */
+	public String getNome() {
+		return nome;
+	}
+	
+	/**
+	 * Esse método adiciona um nome digitado na caixa de texto e atribui a nome
+	 * @param Recebe o nome do valor digitado
+	 */
+	public void setNome(String nome) {
+		if(nome.isBlank()) {
+			throw new IllegalArgumentException("Nome não pode ser vazio!");
+		}else {
+			this.nome = nome;
+		}
+	}
+	
+	/**
+	 * Esse método retorna o tipo do pokemon
+	 * @return retorna o tipo
+	 */
+	public int getTipo() {
+		return tipo;
+	}
+	
+	/**
+	 * Esse método retorna o nivel do pokemon
+	 * @return retorna o nível do pokemon
+	 */
+	public int getNivel() {
+		return nivel;
+	}
 
-public class Pokemons {
+	/**
+	 * Esse método privado que adiciona um nivel determinado para o pokemon
+	 * @param nivel Valor do nível que o usuário digitou
+	 */
+	private void setNivel(int nivel) {
+		this.nivel = nivel;
+	}
 
+	/**
+	 * Esse método retorna a experiencia do pokemon
+	 * @return experiencia do pokemon
+	 */
+	public int getExp() {
+		return exp;
+	}
+	
+	/**
+	 * Esse método adiciona uma experiencia ao pokemon
+	 * @param exp Valor da experiencia do pokemon
+	 */
+	public void setExp(int exp) {
+		
+		this.exp += exp;
+		if(this.exp > 100) {
+			this.exp -= 100;
+			this.nivel++;
+		}
+	}
+	
+	/**
+	 * Esse método adiciona um tipo de pokemon que foi digitado na caixa de texto
+	 * @param tipo Valor do tipo digitado
+	 */
+	public void setTipo(int tipo) {
+		
+		if(tipo > 3) {
+			throw new IllegalArgumentException("Tipo não existente no sistema!");
+		}else {
+			this.tipo = tipo;
+		}
+
+	}
+	/**
+	 * Esse método mostra as informações do Pokemon
+	 * @return Uma String com essas informações 
+	 */
+	@Override
+	public String toString() {
+		return "Pokemon\nNome:" + nome + "\nTipo:" + tipo + "\nNivel:" + nivel + "Exp:" + exp + "\n";
+	}
+	
+	
 }
